f48f6e045959a22905877ebad02521a938c1ee51
Aly El-esawy <73000699+alielesawy@users.noreply.github.com>
2025-05-06 09:38:02 +0000

    Update greeting message in app.py and add CI/CD pipeline configuration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Jenkins! This is a From GORIZM."
+    return "Hello, GitHub Action! This is a From GORIZM."
 
 @app.route('/health')
 def health():
